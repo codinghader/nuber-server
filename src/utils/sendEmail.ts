3d404a4b0d629f78d685332063817a291d555bc3
Jongho Park <jh0x70@gmail.com>
2018-08-28 18:14:32 +0900

    #1.50

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,22 @@
+import MailGun from "mailgun-js";
+
+const mailGunClient = new MailGun({
+  apiKey: process.env.MATILGUN_API_KEY || "",
+  domain: process.env.MAILGUN_DOMAIN || ""
+});
+
+const sendEmail = (subject: string, html: string) => {
+  const emailData = {
+    from: "parkbustour@gmail.com",
+    to: "parktemp@naver.com",
+    subject,
+    html
+  };
+  return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello! ${fullName}, please verify your email`;
+  const emailBody = `Verify your email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`;
+  return sendEmail(emailSubject, emailBody);
+};
